2e8154f4ed69ea38f9dd303bec9687664dea494c
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-01-31 09:52:30 +0800

    commit

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,13 +1,14 @@
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
 import Navbar from "../ui/dashboard/navbar/navbar"
+import styles from "../ui/dashboard/dashboard.module.css"
 
 const Layout = ({children}) => {
   return (
-    <div>
-        <div>
+    <div className={styles.container}>
+        <div className={styles.menu}>
             <Sidebar />
         </div>
-        <div>
+        <div className={styles.content}>
             <Navbar />
             {children}
         </div>
--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,6 +1,89 @@
-import React from "react";
+import styles from "./sidebar.module.css"
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout,
+} from "react-icons/md";
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Revenue",
+        path: "/dashboard/revenue",
+        icon: <MdWork />,
+      },
+      {
+        title: "Reports",
+        path: "/dashboard/reports",
+        icon: <MdAnalytics />,
+      },
+      {
+        title: "Teams",
+        path: "/dashboard/teams",
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
 
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <div className={styles.container}>
+      <ul>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>cat.title</li>
+        ))}
+      </ul>
+    </div>
+  )
 };
 export default Sidebar;
