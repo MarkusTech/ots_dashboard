2d5e6c16bab56ab62920f5a73b7f179c15fc0007
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-01-31 16:06:47 +0800

    commit

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,12 +1,22 @@
 import { MdSupervisedUserCircle } from "react-icons/md"
 import styles from "./card.module.css"
 
-const Card = () => {
+const Card = ({ item }) => {
   return (
     <div className={styles.container}>
-      <MdSupervisedUserCircle />
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Sales</span>
+        <span className={styles.number}>123123</span>
+        <span className={styles.detail}>
+          <span className={1 > 0 ? styles.positive : styles.negative}>
+            2
+          </span>{" "}
+          {1 > 0 ? "more" : "less"} than previous week
+        </span>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Card;
