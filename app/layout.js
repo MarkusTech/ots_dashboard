e34b4be95b5aef3cf0aeb1483bfaa87ca189336e
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-01-31 08:51:05 +0800

    dashboard

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lama Dev Next.js Admin Dashboard',
-  description: 'Next.js Tutorial',
+  title: 'Dashboard Practice',
+  description: 'Dashboard Practice',
 }
 
 export default function RootLayout({ children }) {
---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+        <div>Login page</div>
+    </div>
+  )
+};
+
+export default LoginPage;
