c832e12db44d3a6f742c39887efb2b371eb991f6
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-01-31 09:05:01 +0800

    dashboard

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "../ui/dashboard/sidebar/sidebar"
+import Navbar from "../ui/dashboard/navbar/navbar"
+
+const Layout = ({children}) => {
+  return (
+    <div>
+        <div>
+            <Sidebar />
+        </div>
+        <div>
+            <Navbar />
+            {children}
+        </div>
+    </div>
+  )
+};
+
+export default Layout;
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>page</div>;
+  return <div>Dashboard</div>;
 };
 
 export default Dashboard;
