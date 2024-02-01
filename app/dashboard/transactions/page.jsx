b05991410f2d709b5623a09781042d7e6bac7aea
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-01 15:18:33 +0800

    transaction

--- app/dashboard/transactions/page.jsx
+++ app/dashboard/transactions/page.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import styles from "../../../app/ui/dashboard/dtransaction/transaction.module.css";
+import Search from "../../ui/dashboard/search/search";
+import Pagination from "../../ui/dashboard/pagination/pagination";
+import Link from "next/link";
+import Image from "next/image";
 
 const Transaction = () => {
-  return <div>page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.transaction}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.transactionImage}
+                />
+                Ginamos
+              </div>
+            </td>
+            <td>Description</td>
+            <td>₱,1000,000</td>
+            <td>02.01.2024</td>
+            <td>72</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default Transaction;
