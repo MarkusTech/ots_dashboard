ebc549ab2f5d6e66eb1396dcd8136f0813e2068e
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-01 08:36:56 +0800

    footer

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,19 +1,21 @@
-import Sidebar from "../ui/dashboard/sidebar/sidebar"
-import Navbar from "../ui/dashboard/navbar/navbar"
-import styles from "../ui/dashboard/dashboard.module.css"
+import Sidebar from "../ui/dashboard/sidebar/sidebar";
+import Navbar from "../ui/dashboard/navbar/navbar";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Footer from "../ui/dashboard/footer/footer";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
-        <div className={styles.menu}>
-            <Sidebar />
-        </div>
-        <div className={styles.content}>
-            <Navbar />
-            {children}
-        </div>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
     </div>
-  )
+  );
 };
 
 export default Layout;
--- app/ui/dashboard/footer/footer.jsx
+++ app/ui/dashboard/footer/footer.jsx
@@ -1,7 +1,12 @@
-import styles from "./footer.module.css"
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>WMR.ORG</div>
+      <div className={styles.text}>All rights reserved.</div>
+    </div>
+  );
 };
 
 export default Footer;
