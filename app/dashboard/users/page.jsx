13989ab05540150b72a28738dc76f9ac8a37df4d
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-01 11:14:07 +0800

    users update

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,62 @@
 import styles from "../../ui/dashboard/users/users.module.css";
+import Search from "../../ui/dashboard/search/search";
+import Link from "next/link";
+import Image from "next/image";
 
 const UserPage = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+              </div>
+            </td>
+            <td>wm.recopelacion@gmail.com</td>
+            <td>02.01.2024</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default UserPage;
--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,7 +1,13 @@
-import styles from "./search.module.css"
+import styles from "./search.module.css";
+import { MdSearch } from "react-icons/md";
 
-const Search = () => {
-  return <div>search</div>;
+const Search = ({ placeholder }) => {
+  return (
+    <div className={styles.container}>
+      <MdSearch />
+      <input type="text" placeholder={placeholder} className={styles.input} />
+    </div>
+  );
 };
 
 export default Search;
