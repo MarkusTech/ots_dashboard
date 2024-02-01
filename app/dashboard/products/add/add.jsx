abd3293f083cb644f8ef906ffbfe770381fb80ec
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-01 11:59:34 +0800

    Create add.jsx

---
+++ app/dashboard/products/add/add.jsx
@@ -0,0 +1,11 @@
+import styles from "../../../../app/ui/dashboard/products/products.module.css";
+
+const AddProductPage = () => {
+  return (
+    <div className={styles.container}>
+      <div>Add Product</div>
+    </div>
+  );
+};
+
+export default AddProductPage;
