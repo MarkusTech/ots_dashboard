5185e8d1eef3848dd08e8b7ce2aa19904194d0c7
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-02 12:45:43 +0800

    update

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -1,36 +1,68 @@
 import styles from "../../../../app/ui/dashboard/users/addUser/addUser.module.css";
+import { addUser } from "../../../lib/actions";
 
 const AddUserPage = () => {
   return (
     <div className={styles.container}>
-      <form action="" className={styles.form}>
-        <input type="text" placeholder="Username" name="username" required />
-        <input type="text" placeholder="Email" name="email" />
-        <input type="password" placeholder="Password" name="password" />
-        <input type="text" placeholder="Phone" name="phone" />
-
+      <form action={addUser} className={styles.form}>
+        <input type="text" placeholder="username" name="username" required />
+        <input type="email" placeholder="email" name="email" required />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          required
+        />
+        <input type="phone" placeholder="phone" name="phone" />
         <select name="isAdmin" id="isAdmin">
-          <option value={false} selected>
-            Is Admin?
-          </option>
-          <option value="admin">Admin</option>
+          <option value={false}>Is Admin?</option>
           <option value={true}>Yes</option>
           <option value={false}>No</option>
         </select>
-
         <select name="isActive" id="isActive">
-          <option value={true} selected>
-            Is Active?
-          </option>
+          <option value={true}>Is Active?</option>
           <option value={true}>Yes</option>
           <option value={false}>No</option>
         </select>
-
-        <textarea name="address" id="desc" rows="16"></textarea>
-
-        <button type="submit">Save</button>
+        <textarea
+          name="address"
+          id="address"
+          rows="16"
+          placeholder="Address"
+        ></textarea>
+        <button type="submit">Submit</button>
       </form>
     </div>
+
+    // <div className={styles.container}>
+    //   <form action="" className={styles.form}>
+    //     <input type="text" placeholder="Username" name="username" required />
+    //     <input type="text" placeholder="Email" name="email" />
+    //     <input type="password" placeholder="Password" name="password" />
+    //     <input type="text" placeholder="Phone" name="phone" />
+
+    //     <select name="isAdmin" id="isAdmin">
+    //       <option value={false} selected>
+    //         Is Admin?
+    //       </option>
+    //       <option value="admin">Admin</option>
+    //       <option value={true}>Yes</option>
+    //       <option value={false}>No</option>
+    //     </select>
+
+    //     <select name="isActive" id="isActive">
+    //       <option value={true} selected>
+    //         Is Active?
+    //       </option>
+    //       <option value={true}>Yes</option>
+    //       <option value={false}>No</option>
+    //     </select>
+
+    //     <textarea name="address" id="desc" rows="16"></textarea>
+
+    //     <button type="submit">Save</button>
+    //   </form>
+    // </div>
   );
 };
 
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,13 +1,12 @@
-import styles from "../../ui/dashboard/users/users.module.css";
-import Search from "../../ui/dashboard/search/search";
-import Pagination from "../../ui/dashboard/pagination/pagination";
-import Link from "next/link";
-import Image from "next/image";
-
+import { deleteUser } from "../../lib/actions";
 import { fetchUsers } from "../../lib/data";
-// import { deleteUser } from "../../lib/actions";
+import Pagination from "../../ui/dashboard/pagination/pagination";
+import Search from "../../ui/dashboard/search/search";
+import styles from "../../ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
-const UserPage = async ({ searchParams }) => {
+const UsersPage = async ({ searchParams }) => {
   const q = searchParams?.q || "";
   const page = searchParams?.page || 1;
   const { count, users } = await fetchUsers(q, page);
@@ -57,7 +56,7 @@ const UserPage = async ({ searchParams }) => {
                       View
                     </button>
                   </Link>
-                  <form action="">
+                  <form action={deleteUser}>
                     <input type="hidden" name="id" value={user.id} />
                     <button className={`${styles.button} ${styles.delete}`}>
                       Delete
@@ -74,4 +73,85 @@ const UserPage = async ({ searchParams }) => {
   );
 };
 
-export default UserPage;
+export default UsersPage;
+
+// --------------------------------------------------------------------
+// 2:21
+//
+// import styles from "../../ui/dashboard/users/users.module.css";
+// import Search from "../../ui/dashboard/search/search";
+// import Pagination from "../../ui/dashboard/pagination/pagination";
+// import Link from "next/link";
+// import Image from "next/image";
+
+// import { fetchUsers } from "../../lib/data";
+// // import { deleteUser } from "../../lib/actions";
+
+// const UserPage = async ({ searchParams }) => {
+//   const q = searchParams?.q || "";
+//   const page = searchParams?.page || 1;
+//   const { count, users } = await fetchUsers(q, page);
+
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.top}>
+//         <Search placeholder="Search for a user..." />
+//         <Link href="/dashboard/users/add">
+//           <button className={styles.addButton}>Add New</button>
+//         </Link>
+//       </div>
+//       <table className={styles.table}>
+//         <thead>
+//           <tr>
+//             <td>Name</td>
+//             <td>Email</td>
+//             <td>Created At</td>
+//             <td>Role</td>
+//             <td>Status</td>
+//             <td>Action</td>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {users.map((user) => (
+//             <tr key={user.id}>
+//               <td>
+//                 <div className={styles.user}>
+//                   <Image
+//                     src={user.img || "/noavatar.png"}
+//                     alt=""
+//                     width={40}
+//                     height={40}
+//                     className={styles.userImage}
+//                   />
+//                   {user.username}
+//                 </div>
+//               </td>
+//               <td>{user.email}</td>
+//               <td>{user.createdAt?.toString().slice(4, 16)}</td>
+//               <td>{user.isAdmin ? "Admin" : "Client"}</td>
+//               <td>{user.isActive ? "active" : "passive"}</td>
+//               <td>
+//                 <div className={styles.buttons}>
+//                   <Link href={`/dashboard/users/${user.id}`}>
+//                     <button className={`${styles.button} ${styles.view}`}>
+//                       View
+//                     </button>
+//                   </Link>
+//                   <form action="">
+//                     <input type="hidden" name="id" value={user.id} />
+//                     <button className={`${styles.button} ${styles.delete}`}>
+//                       Delete
+//                     </button>
+//                   </form>
+//                 </div>
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//       <Pagination count={count} />
+//     </div>
+//   );
+// };
+
+// export default UserPage;
