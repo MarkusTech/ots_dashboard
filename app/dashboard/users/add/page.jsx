c1aecd900cf149535ca0cf48b2e38a2d059fafac
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-02 09:08:52 +0800

    update

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -4,11 +4,30 @@ const AddUserPage = () => {
   return (
     <div className={styles.container}>
       <form action="" className={styles.form}>
-        <input type="text" placeholder="Name" name="name" required />
+        <input type="text" placeholder="Username" name="username" required />
         <input type="text" placeholder="Email" name="email" />
-        <input type="text" placeholder="Created At" name="created" />
-        <input type="text" placeholder="Role" name="role" />
-        <input type="text" placeholder="Status" name="status" />
+        <input type="password" placeholder="Password" name="password" />
+        <input type="text" placeholder="Phone" name="phone" />
+
+        <select name="isAdmin" id="isAdmin">
+          <option value={false} selected>
+            Is Admin?
+          </option>
+          <option value="admin">Admin</option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+
+        <select name="isActive" id="isActive">
+          <option value={true} selected>
+            Is Active?
+          </option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+
+        <textarea name="address" id="desc" rows="16"></textarea>
+
         <button type="submit">Save</button>
       </form>
     </div>
