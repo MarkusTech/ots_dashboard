d461ce0871a50f54e8a84a2ce61c2b76cf3c7e57
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-03 11:16:24 +0800

    selected user fetched

--- app/dashboard/transactions/[id]/page.jsx
+++ app/dashboard/transactions/[id]/page.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import styles from "../../../ui/dashboard/dtransaction/singleTransaction/singleTransaction.module.css";
+import Image from "next/image";
+import { fetchTransaction } from "../../../lib/data";
 
-const page = () => {
-  return <div>page</div>;
+const SingleTransactionPage = async ({ params }) => {
+  const { id } = params;
+  const transaction = await fetchTransaction(id);
+
+  const data = transaction;
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src={"/noavatar.png"} alt="" fill />
+        </div>
+        {data.itemName}
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="hidden" name="itemname" value={data.itemName} />
+          <label>Item Name</label>
+          <input type="text" name="itemname" placeholder={data.itemName} />
+          <label>Unit of measure</label>
+          <input type="text" name="uom" placeholder={data.uom} />
+          <label>Category</label>
+          <input type="text" name="category" placeholder={data.category} />
+          <label>Price</label>
+          <input type="text" name="price" placeholder={data.price} />
+          <button>Update</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default SingleTransactionPage;
